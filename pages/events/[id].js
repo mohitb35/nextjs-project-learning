24b0eb27a12aaaf1a78004f8e432f2fa2256cd68
mohitb35 <44917347+mohitb35@users.noreply.github.com>
2023-01-08 15:20:45 +0530

    use SSG for event detail pages
    - add helper to get event details by id
    - prerender featured events at build time (using getStaticPaths)
    - handle not found and loading scenarios for event using fallback

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,14 +1,15 @@
-import { useRouter } from 'next/router';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
-import { getEventById } from '../../dummy-data';
+import { getFeaturedEvents, getEventById } from '../../helpers/api-util';
+import { useRouter } from 'next/router';
 
-function EventDetailsPage() {
+function EventDetailsPage({ event }) {
 	const router = useRouter();
 
-	const eventId = router.query.id;
-	const event = getEventById(eventId);
+	if (router.isFallback) {
+		return <div>Loading...</div>;
+	}
 
 	if (!event) return <div>No Event Found</div>;
 
@@ -26,6 +27,35 @@ function EventDetailsPage() {
 			</EventContent>
 		</>
 	);
+}
+
+export async function getStaticPaths() {
+	const events = await getFeaturedEvents();
+	const paths = events.map((event) => ({
+		params: { id: event.id },
+	}));
+
+	return {
+		paths,
+		fallback: 'blocking', // can also be true or 'blocking'
+	};
+}
+
+export async function getStaticProps(context) {
+	const event = await getEventById(context.params.id);
+
+	if (!event) {
+		return {
+			notFound: true,
+		};
+	}
+
+	return {
+		props: {
+			event,
+		},
+		revalidate: 30,
+	};
 }
 
 export default EventDetailsPage;
